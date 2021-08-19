f271abc38cbc9640033b39711281677c5934b69e
ishtiaqhimel <mdhimelislam007@gmail.com>
2021-08-19 21:07:57 +0600

    Update manacher.cpp

--- String/manacher.cpp
+++ String/manacher.cpp
@@ -16,15 +16,15 @@ void manacher(const string s) {
     p[0] = vector<int>(n+1); ///even len
     p[1] = vector<int>(n);   ///odd len
 
-    for (int z=0; z<2; z++) {
-        for (int i=0, l=0, r=0; i<n; i++) {
+    for(int z = 0; z < 2; z++) {
+        for(int i = 0, l = 0, r = 0; i < n; i++) {
             int t = r - i + !z;
-            if(i<r)
+            if(i < r)
                 p[z][i] = min(t, p[z][l+t]);
             int L = i - p[z][i], R = i + p[z][i] - !z;
-            while(L>=1 && R+1<n && s[L-1]==s[R+1])
+            while((L >= 1) && (R+1 < n) && (s[L-1]==s[R+1]))
                 p[z][i]++, L--, R++;
-            if(R>r)
+            if(R > r)
                 l=L, r=R;
         }
     }
